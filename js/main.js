7f5646ed9e0e3d43d957860221970d175b5d949f
sanjidanahar <cse02707453@gmail.com>
2024-03-02 12:43:14 +0600

    Fixed the mistake

--- js/main.js
+++ js/main.js
@@ -1,94 +1,177 @@
-// buy ticket
-const seats = tag("button", id("seats"));
-const selectedSeats = [];
-const SEAT_PRICE = 550;
-let total_price = 0;
-const coupons = {
-  NEW15: 15,"Couple 20": 20,
-};
+
+let seatSelectionLimit = 0
+let totalSeats = 40
 
 // poribohon
-for (const seat of seats) {
-  let seatName = seat.innerHTML;
-  seat.id = seatName;
-  seat.style.width = id("circle").offsetWidth + "px";
-  seat.onclick = function () {
-    if (selectedSeats.includes(seatName)) {
-      seat.classList.remove("bg-[#1DD100]");
-      selectedSeats.splice(selectedSeats.indexOf(seatName), 1);
-    } else {
-      if (selectedSeats.length < 4) {
-        seat.classList.add("bg-[#1DD100]");
-        selectedSeats.push(seatName);
-      } else {
-        alert("You already have selected 4 seats!");
-      }
-    }
-    const seatsCount = selectedSeats.length;
-    id("seat-count").innerHTML = seatsCount;
-    id("seats-left").innerHTML = 40 - seatsCount;
-    id("selected-list").innerHTML = selectedSeats
-      .map(
-        (selectedSeat) =>
-          `<div class="flex justify-between border-gray-300 text-gray-700"><span>${selectedSeat}</span><span>Economoy</span><span>550</span></div>`
-      )
-      .join("");
-    total_price = seatsCount * SEAT_PRICE;
-    id("total-price").innerHTML = total_price;
-    id("grand-total").innerHTML = total_price;
-    id("discount").innerHTML = 0;
-    if (seatsCount === 4) {
-      id("coupon-apply").removeAttribute("disabled");
-    } else {
-      id("coupon-apply").setAttribute("disabled", "");
-    }
-    applyBtn();
-  };
-}
-id("coupon-apply").onclick = function () {
-  for (const coupon in coupons) {
-    if (coupon === id("coupon").value) {
-      const discount = (coupons[coupon] * total_price) / 100;
-      id("discount").innerHTML = discount;
-      id("grand-total").innerHTML = parseInt(total_price - discount);
-      id("coupon-apply").parentElement.classList.add("hidden");
-      return;
-    }
-  }
-  alert("invalid coupon");
-};
+let seatings = document.querySelectorAll("#seats .btn")
+let seatsLeftCounter = document.getElementById("seats-left")
+let seatCount = document.getElementById("seat-count")
+let checkoutTable = document.querySelector("#checkout-table tbody")
+let totalPrice = document.getElementById("total-price")
+let discount = document.getElementById("discount")
+let couponApply = document.getElementById("coupon-apply")
+let grandTotal = document.getElementById("grand-total")
+let contactFormSubmitBtn = document.getElementById("apply")
+let successMsg = document.getElementById("success-msg")
+let continueBtn = document.getElementById("continue")
 
-id("apply").onclick = function () {
-  if (selectedSeats.length === 0) {
-    alert("Please select a seat!");
-    id("A1").focus();
-  } else if (id("phone").value.toString().length === 0) {
-    alert("Please Enter your phone number");
-    id("phone").focus();
-  } else if (id("name").value.toString().length === 0) {
-    alert("Please Enter your name");
-    id("name").focus();
-  } else {
-    id("done").showModal();
-  }
-};
 
-id("phone").onchange = () => applyBtn();
-id("phone").oninput = () => applyBtn();
-id("name").oninput = () => applyBtn();
-id("name").onchange = () => applyBtn();
-function applyBtn() {
-  if (
-    selectedSeats.length !== 0 &&
-    id("phone").value.toString().length !== 0 &&
-    id("name").value.toString().length !== 0
-  ) {
-    id("apply").classList.add("bg-[#1DD100]");
-  } else {
-    id("apply").classList.remove("bg-[#1DD100]");
-  }
+// remove all data after submit
+let removeData = () => {
+
+  seatSelectionLimit = 0
+  totalSeats = 40
+
+  // remove counters
+  seatsLeftCounter.textContent = "40"
+  seatCount.textContent = "0"
+
+  // remove checkout table data
+  let tableData = document.querySelectorAll("#checkout-table .data")
+  tableData.forEach(function (element) {
+    element.remove();
+  })
+
+  // uncheck seats
+  seatings.forEach((btn) => {
+    if(btn.classList.contains('clicked')){
+      btn.classList.remove('clicked')
+    }
+  })
+
+  // remove checkout data
+  discount.textContent = "0"
+  totalPrice.textContent = "0"
+  grandTotal.textContent = "0"
+
+  // remove contact form data
+  document.getElementById("name").value = 0
+  document.getElementById("phone").value = 0
+  document.getElementById("email").value = 0
+
 }
 
+// remove success msg with continue button
+continueBtn.addEventListener('click', () => {
+  successMsg.classList.add("opacity-0")
+  successMsg.classList.add('invisible')
 
-const id = (e, p = document) => p.getElementById(e);
-const tag = (e, p = document) => p.getElementsByTagName(e);
+  removeData()
+})
+
+// submit contact form
+contactFormSubmitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  let name = document.getElementById("name").value
+  let phone = document.getElementById("phone").value
+
+  if (name == "" || phone == "") {
+    return alert("Please fill all the details")
+  }
+
+  successMsg.classList.remove("opacity-0")
+  successMsg.classList.remove('invisible')
+
+  // remove all data after submit
+  removeData()
+
+  setTimeout(() => {
+    successMsg.classList.add("opacity-0")
+    successMsg.classList.add('invisible')
+  }, 5000)
+
+})
+
+// seat checkout info markup
+let markup = (id) => {
+  let htmlData = `
+    <tr class="data font-normal text-gray-500" id="${id}">
+      <td class="text-left uppercase">${id}</td>
+      <td class="text-center"><span>Economy</span></td>
+      <td class="text-right"><span>550</span></td>
+    </tr>
+  `
+
+  return htmlData
+}
+
+// coupon apply
+couponApply.addEventListener('click', (e) => {
+  e.preventDefault()
+  let couponCode = document.getElementById("coupon").value
+  if (couponCode.trim() == "") return alert("please apply a coupon")
+  checkoutCalculator(couponCode)
+})
+
+// coupon check
+let coupon = (code, price) => {
+
+  if (code == "NEW15") {
+    return (15 / 100) * price
+  }
+
+  if (code == "Couple 20") {
+    return (20 / 100) * price
+  }
+
+  return alert("vai code nai")
+}
+
+// checkout calculator
+let checkoutCalculator = (couponCode) => {
+  let price = seatSelectionLimit * 550
+
+  if (couponCode) {
+    var discountPrice = coupon(couponCode, price)
+    discount.textContent = "" + discountPrice
+  }
+
+  totalPrice.textContent = "" + price
+  if (discountPrice) return grandTotal.textContent = "" + (price - discountPrice)
+  grandTotal.textContent = "" + price
+}
+
+
+// look over all the buttons
+seatings.forEach((btn) => {
+
+  // check for any click event of the buttons
+  btn.addEventListener('click', (e) => {
+
+    // if clicked before
+    if (btn.classList.contains('clicked')) {
+      seatSelectionLimit--
+      totalSeats++
+      seatsLeftCounter.textContent = "" + totalSeats
+      seatCount.textContent = "" + seatSelectionLimit
+
+      let generatedElement = document.getElementById("" + e.target.value)
+      if (generatedElement) generatedElement.remove()
+
+      checkoutCalculator() // call calculator function
+      if (seatSelectionLimit < 4) couponApply.disabled = true
+
+      return btn.classList.remove("clicked")
+    }
+
+    // if not clicked before
+    if (seatSelectionLimit < 4) {
+      seatSelectionLimit++
+      totalSeats--
+
+      seatsLeftCounter.textContent = "" + totalSeats
+      seatCount.textContent = "" + seatSelectionLimit
+
+      checkoutTable.insertAdjacentHTML("beforeend", markup(e.target.value)) // insert html
+      checkoutCalculator() // call calculator function
+
+      // enable apply button
+      if (seatSelectionLimit == 4) couponApply.removeAttribute("disabled")
+
+      return btn.classList.add("clicked")
+    }
+
+    alert("You have selected 4 tickets")
+  })
+})
--- js/utility.js
+++
@@ -1,2 +0,0 @@
-const id = (e, p = document) => p.getElementById(e);
-const tag = (e, p = document) => p.getElementsByTagName(e);
